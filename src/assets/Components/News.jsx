39f41686bbdd3934566bd38772ed6d78db228952
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-23 19:18:49 +0530

    news web app

--- src/assets/Components/News.jsx
+++ src/assets/Components/News.jsx
@@ -39,7 +39,7 @@ useEffect(()=>{
         <div className="Som">
           <div className="trending-news">
             <h1>Sana</h1>
-            <h1 className="Sal">News</h1>
+            <h1 className="Sal">News WEB APP</h1>
           </div>
 
           <ul>
